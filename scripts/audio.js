143e5c66b28a68db442cc24e60e718be260013c8
Darren Hebner <darrenwilliamhebner@gmail.com>
2015-11-28 15:56:58 -0500

    added pitches and chords to audio.js

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,16 +1,23 @@
 window.onload = init();
-var audioCtx, oscillator, oscillator2, oscillator3, gainNode, biquadFilter;
+var audioCtx, oscillator, oscillator2, oscillator3, oscillator4, oscillator5, gainNode, biquadFilter;
 
 function init() {
 	audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 	oscillator = audioCtx.createOscillator();
 	oscillator2 = audioCtx.createOscillator();
 	oscillator3 = audioCtx.createOscillator();
+	oscillator4 = audioCtx.createOscillator();
+	oscillator5 = audioCtx.createOscillator();
+
 	gainNode = audioCtx.createGain();
+	gainNode.gain.value = 0.03; //reduce the volume by half
 
 	oscillator.connect(gainNode);
 	oscillator2.connect(gainNode);
 	oscillator3.connect(gainNode);
+	oscillator4.connect(gainNode);
+	oscillator5.connect(gainNode);
+
 	gainNode.connect(audioCtx.destination);
 
 	biquadFilter = audioCtx.createBiquadFilter();
@@ -18,7 +25,7 @@ function init() {
 	biquadFilter.frequency.value = 1000;
 	biquadFilter.gain.value = 25;
 
-	oscillator.type = 'sine'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+	oscillator.type = 'sawtooth'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
 	oscillator.frequency.value = 440; // value in hertz
 	oscillator.start();
 
@@ -26,10 +33,33 @@ function init() {
 	oscillator2.frequency.value = 550;
 	oscillator2.start();
 
-	oscillator3.type = 'sine';
+	oscillator3.type = 'sawtooth';
 	oscillator3.frequency.value = 550;
 	oscillator3.start();
+
+	oscillator4.type = 'sine';
+	oscillator4.frequency.value = 550;
+	oscillator4.start();
+
+	oscillator5.type = 'sawtooth';
+	oscillator5.frequency.value = 550;
+	oscillator5.start();
 }
+
+var notes = {
+			g2: 98,
+			b3: 246.9,
+			d4: 293.7,
+			fSharp4: 370,
+			a4: 440,
+			a2: 110,
+			a3: 220,
+			cSharp4: 277.2,
+			e4: 329.6,
+			b2: 123.5,
+			g4: 392
+		};
+
 
 
 var dir;
@@ -39,14 +69,32 @@ socket.on('message', function(data){
 });
 socket.on('deviceData', function(data){
 	var lr = Math.ceil(data.lr);
-	var fb = Math.ceil(data.fb);
+	var fb = Math.ceil(data.fb) + 90; // to offset the -90deg
 	dir = Math.ceil(data.dir);
 	$("#lr").text(lr);
 	$("#fb").text(fb);
 	$("#dir").text(dir);
 
-	oscillator.frequency.value = fb * 10;
-	oscillator2.frequency.value = (fb * 10) * 1.25; // major third
-	oscillator3.frequency.value = ((fb * 10) * 1.25) * 1.189; // minor third
+	setTimeout(function() {
+		if ( fb < 60) {
+			oscillator.frequency.value = notes.g2;
+			oscillator2.frequency.value = notes.b3;
+			oscillator3.frequency.value = notes.d4;
+			oscillator4.frequency.value = (notes.fSharp4 - oscillator4.frequency.value) * 0.1;
+			oscillator5.frequency.value = (notes.g4 - oscillator5.frequency.value) * 0.1;
+		} else if ( fb > 60 && fb < 120 ) {
+			oscillator.frequency.value = notes.b2;
+			oscillator2.frequency.value = notes.b3;
+			oscillator3.frequency.value = notes.d4;
+			oscillator4.frequency.value = (notes.fSharp4 - oscillator4.frequency.value) * 0.1;
+			oscillator5.frequency.value += (notes.a4 - oscillator5.frequency.value) * 0.1;
+		} else if ( fb > 120 ) {
+			oscillator.frequency.value = notes.a2;
+			oscillator2.frequency.value = notes.a3;
+			oscillator3.frequency.value = notes.cSharp4;
+			oscillator4.frequency.value = (notes.e4 - oscillator4.frequency.value) * 0.1;
+			oscillator5.frequency.value = (notes.fSharp4 - oscillator5.frequency.value) * 0.1;
+		}
+	}, 60);
 	biquadFilter.frequency.value = lr;
 })
--- scripts/visualization.js
+++ scripts/visualization.js
@@ -1,47 +1,47 @@
 var scene = new THREE.Scene();
-		var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-		var renderer = new THREE.WebGLRenderer({alpha: true});
-		renderer.setClearColor( 0xEEEEEE, 1);
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		document.body.appendChild( renderer.domElement );
+var renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setClearColor( 0xEEEEEE, 1);
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-		var dir;
-		var fb = 4;
-		var socket = io.connect();
-		socket.on('deviceData', function(data){
-			var lr = Math.ceil(data.lr);
-			fb = Math.ceil(data.fb);
-			dir = Math.ceil(data.dir);
-			setTimeout(function() {
-				plane.rotation.z += ((dir * 0.06) - plane.rotation.z)*0.05;
-			}, 60);
-		})
+var dir;
+var fb = 4;
+var socket = io.connect();
+socket.on('deviceData', function(data){
+	var lr = Math.ceil(data.lr);
+	fb = Math.ceil(data.fb);
+	dir = Math.ceil(data.dir);
+	setTimeout(function() {
+		plane.rotation.z += ((dir * 0.02) - plane.rotation.z)*0.05;
+	}, 60);
+})
 
-		var geometry = new THREE.PlaneGeometry( 60, 60, 20, 20 );
-		geometry.__dirtyVertices = true;
-		geometry.dynamic = true;
+var geometry = new THREE.PlaneGeometry( 60, 60, 20, 20 );
+geometry.__dirtyVertices = true;
+geometry.dynamic = true;
 
-		for (var i = 0, l = geometry.vertices.length; i < l; i++) {
-		  geometry.vertices[i].z = fb * Math.random();
-		}
-		
+for (var i = 0, l = geometry.vertices.length; i < l; i++) {
+  geometry.vertices[i].z = fb * Math.random();
+}
 
-		var material = new THREE.MeshBasicMaterial({
-	        color : 0x666666,
-	        wireframe : true
-    	});
 
-		var plane = new THREE.Mesh( geometry, material );
-		scene.add( plane );
-		plane.rotation.x = 90;
+var material = new THREE.MeshBasicMaterial({
+    color : 0x666666,
+    wireframe : true
+});
 
-		camera.position.z = 60;
+var plane = new THREE.Mesh( geometry, material );
+scene.add( plane );
+plane.rotation.x = 90;
 
-		console.log(plane);
+camera.position.z = 60;
 
-		function render() {
-			requestAnimationFrame( render );
-			renderer.render( scene, camera );
-		}
-		render();
+console.log(plane);
+
+function render() {
+	requestAnimationFrame( render );
+	renderer.render( scene, camera );
+}
+render();
